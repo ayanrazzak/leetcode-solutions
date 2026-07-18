0a609a0cc85425706a48d35be56f39450820345b
ayanrazzak <ayanrazzak04@gmail.com>
2026-07-18 18:13:42 +0530

    Time: 0 ms (100%), Space: 10 MB (37.59%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int begin = 0;
+        int end = s.size()-1;
+        while(begin<end){
+            if(!isalnum(s[begin])){
+                begin++;
+            }
+            else if(!isalnum(s[end])){
+                end--;
+            }
+            else{
+                if(tolower(s[begin]) != tolower(s[end])){
+                    return false;
+                }
+                begin++;
+                end--;
+            }
+        }
+        return true;
+        return 0;
+    }
+};
